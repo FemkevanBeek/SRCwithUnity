583698c96a1acd6adba19501a6a2b192c09d21b9
FemkevanBeek <79458950+FemkevanBeek@users.noreply.github.com>
2021-11-22 15:24:50 +0100

    Basic Matlab-Unity communication

---
+++ SRC_CommunicationOnly/Assets/Scripts/ClientCommunication/ComposeMessage.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Text;
+
+public class ComposeMessage : MonoBehaviour
+{
+
+    //this thread handles continuous outgoing data
+    static float updateTimestep;
+    static float timeStarted;
+    static float update = 0.0f;
+    //ramp
+    static float timeHighSignalRamp = 4.0f;
+    //step
+    static float timeHighSignalStep = 4.0f;
+    //sine
+    static float timeHighSignalSine = 15.0f;
+    //pneunet
+    static float timeHighSignalPneu = 5.0f;
+
+    static float timeHighSignal;
+    static float timeStartSignal = 3.0f;
+    static float timeCurrent;
+
+    //TO DO: think about if we can initialize this with a setting that says something about the length of the signal
+    static double[] currentSignal;
+
+    //initialize simulation variables
+    int selectorVariable;
+    static double simSignal;
+    bool sendingActivated = false;
+    bool handReady = false;
+    static int counter = 0;
+    StringBuilder sb = new StringBuilder();
+
+    private void Start()
+    {
+            updateTimestep = 1.0f / Settings.updateFrequency;
+
+            currentSignal = new double[Settings.fingerAngleIndex.Length];
+
+        if (Settings.storingData == true)
+        {
+            Settings.filePath = Settings.filePath + DateTime.Now.ToString("yyyy-mm-dd-hh-mm-ss") + ".txt";
+
+            //StringBuilder sb = new StringBuilder();
+            sb.Append("Time [s] , Frame nr");
+            for (int i = 0; i < Settings.fingerAngleIndex.Length; i++)
+            {
+                sb.Append(",");
+                sb.Append($"Sent pressure {Settings.fingerAngleIndex[i]}");
+            }
+
+            if (Settings.receivingData == true)
+            {
+                for (int i = 0; i < Settings.fingerAngleIndex.Length; i++)
+                {
+                    sb.Append(",");
+                    sb.Append($"Received pressure {Settings.fingerAngleIndex[i]}");
+                }
+            }
+            sb.AppendLine();
+        }
+
+        timeStarted = Time.realtimeSinceStartup;
+    }
+
+    private void Update()
+    {
+            if (ClientHandle.welcomeAccepted)
+            {
+
+                //if ((Input.GetKeyDown("space") && handReady == true && Settings.startOnPlay==false))
+                //{
+                //    if (sendingActivated == false)
+                //    {
+                //        sendingActivated = true;
+                //        AudioSource audio = GetComponent<AudioSource>();
+                //        audio.clip = spacePressedClip;
+                //        audio.Play();
+                //        print("System started");
+                //    }
+                //    else if (sendingActivated == true)
+                //    {
+                //        sendingActivated = false;
+                //        AudioSource audio = GetComponent<AudioSource>();
+                //        audio.clip = pausedClip;
+                //        audio.Play();
+                //        print("Tele-operation paused");
+
+                //        for (int i = 0; i < Settings.fingerAngleIndex.Length; i++)
+                //        {
+                //            currentSignal[i] = 0;
+                //        }
+                //        //Send zeros to system to put it at resting state
+                //        SendInputToServer(currentSignal);
+                //    }
+                //}
+
+                if(Settings.startOnPlay == true)
+            {
+                sendingActivated = true;
+            }
+
+            //if (Input.GetKeyDown("space") && timerStarted == false)
+            //{
+             //   
+              //  timerStarted = true;
+            //}
+
+            update += Time.deltaTime;               //this controls the timing of the loop
+            if (update > updateTimestep)
+            {
+                if (!Settings.simulateLeap)
+                {
+                    for (int i = 0; i < Settings.fingerAngleIndex.Length; i++)
+                    {
+                        currentSignal[i] = 0;
+                    }
+
+                }
+                else
+                {
+
+                    simSignal = 0.52f;
+
+                    timeCurrent = Time.realtimeSinceStartup - timeStarted;
+
+                    if (timeCurrent > timeStartSignal) {
+
+                        if (Settings.stepInput == true)
+                        {
+                            simSignal = 0.625f;
+                            timeHighSignal = timeHighSignalStep;
+                        }
+
+                        if(Settings.rampInput == true)
+                        {
+                            simSignal = 0.52f + 0.1f*(timeCurrent - timeStartSignal);
+                            timeHighSignal = timeHighSignalRamp;
+                            //counttime++;
+                        }
+
+                        if (Settings.sineInput == true)
+                        {
+                            simSignal = 0.57 + 0.05f * Mathf.Sin(Mathf.PI * Settings.simulationFrequency * (timeCurrent - timeStartSignal));
+                            timeHighSignal = timeHighSignalSine;
+                        }
+
+                        if (Settings.pneuInput == true)
+                        {
+                            simSignal = 0.52f + 0.09f * (timeCurrent - timeStartSignal);
+                            timeHighSignal = timeHighSignalPneu;
+                        }
+
+                        if (timeCurrent > timeHighSignal)
+                        {
+                            simSignal = 0.52f;
+                        }
+
+                    }
+
+                    for (int i = 0; i < Settings.fingerAngleIndex.Length; i++)
+                    {
+                        currentSignal[i] = (double)simSignal;
+                    }
+
+                    //Debug.Log($"timerStarted is {timerStarted}");
+                }
+
+                if (sendingActivated == true)
+                {
+                    SendInputToServer(currentSignal);
+
+                    if (Settings.dataToConsole)
+                    {
+                        for (int i = 0; i < Settings.fingerAngleIndex.Length; i++)
+                        {
+                            Debug.Log($"Trying to send: {currentSignal[i]} for finger {Settings.fingerAngleIndex[i]}");
+                        }
+                    }
+
+                }
+                
+                if(Settings.storingData==true)
+                { 
+               // StringBuilder sb = new StringBuilder();
+                
+                    sb.Append(Time.realtimeSinceStartup);
+                    sb.Append(",");
+                    sb.Append(counter);
+
+                    for (int index = 0; index < currentSignal.Length; index++)
+                    {
+                        sb.Append(",");
+                        sb.Append(currentSignal[index]);
+
+                        if (Settings.receivingData == true)
+                        {
+                            sb.Append(",");
+                            sb.Append(ClientHandle.pressureReceived[index]);
+                        }
+                    }
+                    sb.AppendLine();
+                    
+                }
+
+                update = 0.0f;
+                counter ++;
+                }
+            }
+
+    }
+
+    private void SendInputToServer(double[] _pressureMessage)
+    {
+        ClientSend.SendPressure(_pressureMessage);
+        //Debug.Log("Sending out pressure");
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (!File.Exists(Settings.filePath))
+        {
+            File.WriteAllText(Settings.filePath, sb.ToString());
+        }
+        else
+        {
+            File.AppendAllText(Settings.filePath, sb.ToString());
+        }
+        Debug.Log("Quitting");
+            //controller.StopConnection();
+            //controller.Dispose();
+    }
+}
---
+++ SRC_CommunicationOnly/Assets/Scripts/ClientCommunication/Settings.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+    //make sure ip and port are the same as the ones in Simulink
+    //127.0.0.1 means local
+    public string _ip = "127.0.0.1";
+    //port 26950 is usually open
+    public int _port = 26950;
+    //delayed start, or start directly when program is run
+    public bool _startOnPlay = true;
+    //Set to true to send out data to Matlab
+    public bool _sendingData = true;
+    //Set to true to receive data from Matlab
+    public bool _receivingData = false;
+    //Set to true to send out data to Matlab
+    public bool _storingData = false;
+    //step input for performance testing
+    public bool _stepInput;
+    //ramp input for performance testing
+    public bool _rampInput;
+    //sine input for performance testing
+    public bool _sineInput;
+    //ramp input for Pneunet performance testing
+    public bool _pneuInput;
+    //Path of data file
+    public string _filePath = "Data/Testfile";
+    //Update frequency for pressurecontrol loop
+    public float _updateFrequency = 120.0f;
+    //Are we printing data to the console on each tick
+    public bool _dataToConsole = true;
+    //Are we printing collision data to the console
+    public bool _collisionToConsole = true;
+    //If checked, we simulate leap motion data
+    public bool _simulateLeap = false;
+    //frequency of simulated block signal
+    public float _simulationFrequency = 0.25f;
+    //index of finger(s) that we're tracking. Numbered 0-4, thumb-pinky
+    public int[] _fingerAngleIndex = new int[] { 0, 1, 2 };
+
+    //initialize static copy of fields, because we can only display non-static fields in Unity
+    public static string ip;
+    public static int port;
+    public static bool startOnPlay;
+    public static bool sendingData;
+    public static bool receivingData;
+    public static bool storingData;
+    public static bool stepInput;
+    public static bool rampInput;
+    public static bool sineInput;
+    public static bool pneuInput;
+    public static string filePath;
+    public static float updateFrequency;
+    public static bool dataToConsole;
+    public static bool collisionToConsole;
+    public static bool simulateLeap;
+    public static float simulationFrequency;
+    public static int[] fingerAngleIndex;
+
+    private void Awake()
+    {
+        ip = _ip;
+        port = _port;
+        startOnPlay = _startOnPlay;
+        updateFrequency = _updateFrequency;
+        dataToConsole = _dataToConsole;
+        collisionToConsole = _collisionToConsole;
+        simulateLeap = _simulateLeap;
+        sendingData = _sendingData;
+        receivingData = _receivingData;
+        storingData = _storingData;
+
+        stepInput = _stepInput;
+        rampInput = _rampInput;
+        sineInput = _sineInput;
+        pneuInput = _pneuInput;
+
+    filePath = _filePath;
+        simulationFrequency = _simulationFrequency;
+        fingerAngleIndex = _fingerAngleIndex;
+
+    }
+}
